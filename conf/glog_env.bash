aa1f1afb39288596384fa0bf475608f357bc721b
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-12-05 11:46:24 -0500

    glog - Initial commit for the Global Log project. 	new file:   .gitignore 	new file:   CHANGELOG.md 	new file:   LICENSE 	new file:   README.md 	new file:   conf/glog_env.bash 	new file:   doc/intro.md 	new file:   project.clj 	new file:   src/glog/core.clj 	new file:   test/glog/core_test.clj

---
+++ conf/glog_env.bash
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+########################################################################
+# Tired of searching for log content and redirect and the like.
+# Centralize and uniformize
+########################################################################
+
+
+# Change that filename if you need to change the log file location
+export ___glog_file="$CACHEDIR/glog.json"
+
+# Change the glog context for easy filtering
+export ___glog_context="glog_env"
+
+function ___glog_init() {
+    if [ ! -f ${___glog_file} ]; then
+        export ___glog_initial_pass=true
+        touch $___glog_file
+    else
+        export ___glog_initial_pass=false
+    fi
+}
+
+#
+# The standard header of a json glog element
+function glog_json_open() {
+    printf "{\"ts\": \"$(date --utc +"%m %d %Y %H:%M:%S")\""
+}
+export -f glog_json_open
+
+#
+# The standard footer of a json glog element
+function glog_json_close() {
+    printf ", \"wfm_cloudappname\": \"${wfm_cloudappname}\""
+    printf ", \"simulation\": \"%s\"" "${simulation}"
+    printf ", \"db_dump\": \"%s\"" "$db_dump"
+    printf ", \"context\": \"${___glog_context}\"}\n"
+}
+export -f glog_json_close
+
+#
+# define a key val entry for a file
+function glog_json_key_file() {
+    key=$1
+    file=$2
+    event=$3
+
+    glog_json_open
+    printf ", \"event\": \"%s\"" "$event"
+    printf ", \"key\": \"%s\"" "$key"
+    printf ", \"val\": {\"file\":   \"%s\"}" "$file"
+    glog_json_close
+}
+export -f glog_json_key_file
+
+
+#
+# Public functions
+function ___glog_pad_________() {
+    pad_short=$(printf '%0.1s' "-"{1..8})
+
+    glog_json_open                          >> ${___glog_file} 2>&1
+    printf ", \"pad\": \"%s\"" "$pad_short" >> ${___glog_file} 2>&1
+    glog_json_close                         >> ${___glog_file} 2>&1
+}
+
+function ___glog_obsolete() {
+    key=$1
+    msg=$2
+    glog_json_open  >> ${___glog_file} 2>&1
+    printf ", \"key\": \"%s\"" "$key" >> ${___glog_file} 2>&1
+    printf ", \"val\": {\"msg\":   \"%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\"}" "$msg" >> ${___glog_file} 2>&1
+    glog_json_close  >> ${___glog_file} 2>&1
+}
+
+function ___glog() {
+    key=$1
+    msg=$2
+    glog_json_open  >> ${___glog_file} 2>&1
+    printf ", \"key\": \"%s\"" "$key" >> ${___glog_file} 2>&1
+    printf ", \"val\": {\"msg\":   \"%s\"}" "$msg" >> ${___glog_file} 2>&1
+    glog_json_close  >> ${___glog_file} 2>&1
+}
+
+function ___glog_redirect() {
+    glog_json_key_file "$1" "$2" "start" >> ${___glog_file} 2>&1
+    "$1"                                 >> "$2" 2>&1
+    glog_json_key_file "$1" "$2" "end"   >> ${___glog_file} 2>&1
+}
+
+export -f ___glog_init
+export -f ___glog_pad_________
+export -f ___glog
+export -f ___glog_redirect
+
+___glog_init
---
+++ test/glog/core_test.clj
@@ -0,0 +1,7 @@
+(ns glog.core-test
+  (:require [clojure.test :refer :all]
+            [glog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
