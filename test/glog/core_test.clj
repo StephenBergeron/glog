115a722db108ef193321240fab3f2c2568536db6
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-12-05 15:38:50 -0500

    Start implementation based on the basic principles. 	modified:   project.clj 	modified:   test/glog/core_test.clj

--- test/glog/core_test.clj
+++ test/glog/core_test.clj
@@ -1,7 +1,31 @@
 (ns glog.core-test
-  (:require [clojure.test :refer :all]
-            [glog.core :refer :all]))
+  (:require [clojure.test :as t]
+            [glog.core :as sut]
+            [clojure.set :as set]
+            [clojure.data.json :as json]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def all-records
+  (map json/read-str (clojure.string/split (slurp "/home/stn/cache/glog.json") #"\n")))
+
+(def common-keys
+  (reduce set/intersection (map (fn [x] (into #{} (keys x))) all-records)))
+
+;; (def header-construct
+;;   (let [s1 all-records
+;;         s2 all-records]))
+
+
+(t/deftest slurp-1-test
+  (t/testing
+      (t/is (= clojure.lang.LazySeq (type all-records)))))
+
+(t/deftest slurp-3-test
+  (t/testing
+      (t/is (= 69 (count all-records)))))
+
+(t/deftest slurp-5-test
+  (t/testing
+      (t/is (=
+             #{"ts" "db_dump" "context" "simulation" "wfm_cloudappname"}
+             common-keys))))
