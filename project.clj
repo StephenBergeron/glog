4fbfc4f9c41f5bcfd52bd9343a7905339bd69b37
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-12-05 19:47:11 -0500

    Use a TDD approach.  Build up te function alongside unit test.  At this point I envision running the whole postpressing pipeline as a set of unit tests. 	modified:   project.clj 	modified:   test/glog/core_test.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]]
+  :plugins [[lein-auto "0.1.3"]
+            [lein-ancient "0.6.8"]]
   :main ^:skip-aot glog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/glog/core_test.clj
+++ test/glog/core_test.clj
@@ -4,28 +4,41 @@
             [clojure.set :as set]
             [clojure.data.json :as json]))
 
-
-(def all-records
-  (map json/read-str (clojure.string/split (slurp "/home/stn/cache/glog.json") #"\n")))
+(def glog
+  "The glog file content in a tabloid data structure."
+  (map json/read-str
+       (clojure.string/split
+        (slurp (System/getenv "___glog_file")) #"\n")))
 
 (def common-keys
-  (reduce set/intersection (map (fn [x] (into #{} (keys x))) all-records)))
+  "Each glog entries have a common set of keys"
+  (reduce set/intersection (map (fn [x] (into #{} (keys x))) glog)))
+
+(defn glog-header
+  "Extract the header for the current datum"
+  [previous current]
+  ;; Everything that belongs to the common-keys
+  ;; and that is different from the previous
+  ;; is assembled into the glog-header
+  nil)
 
 ;; (def header-construct
-;;   (let [s1 all-records
-;;         s2 all-records]))
+;;   (let [s1 glog
+;;         s2 glog]))
 
 
-(t/deftest slurp-1-test
-  (t/testing
-      (t/is (= clojure.lang.LazySeq (type all-records)))))
+(t/deftest glog-type-test
+  (t/testing "The tabloid structure is a lazy sequence of some Map."
+    (t/is (= clojure.lang.LazySeq (type glog)))
+    (t/is (= clojure.lang.PersistentArrayMap (type (last glog))))))
 
-(t/deftest slurp-3-test
-  (t/testing
-      (t/is (= 69 (count all-records)))))
 
-(t/deftest slurp-5-test
-  (t/testing
-      (t/is (=
-             #{"ts" "db_dump" "context" "simulation" "wfm_cloudappname"}
-             common-keys))))
+(t/deftest glog-contains-data-test
+  (t/testing "Ensure that we have some data in the glog."
+    (t/is (> (count glog) 10))))
+
+(t/deftest common-keys-are-well-known-test
+  (t/testing "The common keys are well known"
+    (t/is (=
+           #{"ts" "db_dump" "context" "simulation" "wfm_cloudappname"}
+           common-keys))))
