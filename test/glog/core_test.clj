ab130fc70d06678d66e2aaea3c1b4552b267e260
Bergeron, Stephen <Stephen.Bergeron@kronos.com>
2017-12-12 11:05:09 -0500

    Even better now. We keep a purely potprocess view on the whole workflow.

--- test/glog/core_test.clj
+++ test/glog/core_test.clj
@@ -31,18 +31,25 @@
     (if (< p1 (count header))
         (let [h0 (nth header p0)
               h1 (nth header p1)
-              [delta1 delta2 delta3]  (data/diff h0 h1)
-              epsilon  (if (nil? delta2) delta2 (into delta2 {"header" true}))]
+              [_ delta _]  (data/diff h0 h1)
+              epsilon (if (nil? delta) delta (into delta {"header" true "emphasize" "-----"}))]
           (recur p1 (+ p1 1) (conj acc epsilon)))
         (lazy-seq acc))))
+
+(def glog-preproc
+  (interleave header-visibility details))
+
+
 
 (t/deftest adhoc-test
   (t/testing "Run some adhoc evaluation."
     (do
-      (printf "header: %n")
-      (print header)
-      (printf "header-visibility: %n")
-      (clojure.pprint/pprint header-visibility)
+      ;(printf "header: %n")
+      ;(print header)
+      (printf "interleave: %n")
+      (clojure.pprint/pprint glog-preproc)
+      ;(printf "header-visibility: %n")
+      ;(clojure.pprint/pprint header-visibility)
       ;; (printf "tdh240: %n")
       ;; (clojure.pprint/pprint tdh240)
       ;; (printf "detail: %n")
@@ -52,11 +59,27 @@
       (t/is (> 42 13 ))
       )))
 
+
+(t/deftest glog-preproc-test
+  (t/testing "The cardinality of glog-preproc is twice the original tabloid."
+    (t/is (= (count glog-preproc) (* 2 (count glog))))))
+
+(t/deftest cardinality-interleave-test
+  (t/testing "The cardinality of the interleave tabloid is the same as the header and the details."
+    (t/is (= (count (interleave header details)) (+ (count header) (count details))))))
+
+(t/deftest cardinality-header-test
+  (t/testing "The cardinality of the header tabloid is the same as the original tabloid."
+    (t/is (= (count glog) (count header) ))))
+
+(t/deftest cardinality-detail-test
+  (t/testing "The cardinality of the details tabloid is the same as the original tabloid."
+    (t/is (= (count glog) (count details)))))
+
 (t/deftest glog-type-test
   (t/testing "The tabloid structure is a lazy sequence of some Map."
     (t/is (= clojure.lang.LazySeq (type glog)))
     (t/is (= clojure.lang.PersistentArrayMap (type (last glog))))))
-
 
 (t/deftest glog-contains-data-test
   (t/testing "Ensure that we have some data in the glog."
